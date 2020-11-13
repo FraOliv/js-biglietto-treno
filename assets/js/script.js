e8904fe6c2377f38333b90b344670ff5354f5547
FRANCESCO <OLIVOTTI>
2020-11-13 16:39:58 +0100

    esercizio completato 1.0

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//va applicato uno sconto del 20% per i minorenni
+//va applicato uno sconto del 40% per gli over 65
+
+
+var TrainTripKM = Number(prompt("Quanti Chilometri vuoi percorrere?"));
+var Age = Number(prompt("How old are you?"));
+if (Age !== Number){
+    alert("You are a Noob")
+}
+//costo per KM
+var CostKm = 0.21;
+var Price = TrainTripKM *CostKm;
+if (Age >= 65) {
+    Price = Price - (Price * 0.4);
+}
+else if ( Age <= 17){
+    Price = Price - (Price * 0.2);
+}
+
+
+document.getElementById("Price-train").innerHTML = Price;
